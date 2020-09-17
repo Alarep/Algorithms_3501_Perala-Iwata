0a84c96f7da3b7d54f7af596fcf47203f77390d5
Alarep <peralaj94@gmail.com>
2020-09-17 13:41:29 -0500

    Set up the Company class with parameters to find the desired programmer and check if the company has an open position.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Test");
+    }
+
+    static void linkAndMatch(){
+        Scanner sc = new Scanner(System.in);
     }
 }
