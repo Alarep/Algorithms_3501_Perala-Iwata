9d394927921fb84648125920950dfdaef69001f9
Machi <iwata008@morris.umn.edu>
2020-09-28 19:12:29 -0500

    Create a new class "FindSatisfactory.java"
    I created this class to clean the main class.
    The color of the file is showing red but I don't know why it's returning it like that.

--- src/Company.java
+++ src/Company.java
@@ -1,13 +1,18 @@
-public class Company {
+public class Company extends Main {
 
     // The parameter for what programmer is preferred by the company.
-    private char preferred;
+    private String preferred;
 
     // The parameter to check if the company has a programmer assigned.
     private boolean isFree;
 
-    Company(char desired, boolean openPos){
+    private int length = 5;
+
+    /*Company(String desired, boolean openPos){
         this.preferred = desired;
         this.isFree = openPos;
-    }
+
+
+    }*/
+
 }
